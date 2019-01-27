b07ef003bd3bdcc7fcc70d7d1112647f65d55c67
Hemachandar V <v.hemachandar@gmail.com>
2019-01-27 15:21:29 +0530

    func: add JSON parser for input file

--- adventure/parser.go
+++ adventure/parser.go
@@ -1,10 +1,17 @@
 package adventure
 
+import (
+	"encoding/json"
+	"os"
+)
+
 /*
 Option contains the next arc and its description
 from a specific arc.
 */
 type Option struct {
+	Arc         string `json:"arc"`
+	Description string `json:"text"`
 }
 
 /*
@@ -13,13 +20,24 @@ definition of the arc and possible paths
 from this arc.
 */
 type Story struct {
+	Title   string   `json:"title"`
+	Content []string `json:"story"`
+	Paths   []Option `json:"options"`
 }
 
 /*
 ParseJSON parses the stringified adventure "data" and
-returns a struct of type "Story" that contains the
-details from input data
+returns a map where Key is the arc-name and value is the
+corresponsing Story struct that contains the
+details from input data.
 */
-func ParseJSON(data string) (adventure []Story) {
+func ParseJSON(filename string) (adventure map[string]Story, err error) {
+	f, err := os.Open(filename)
+	defer f.Close()
+	if err != nil {
+		return
+	}
+	d := json.NewDecoder(f)
+	err = d.Decode(&adventure)
 	return
 }
--- server.go
+++ server.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/hemv/cyoa/adventure"
+)
+
+const (
+	adventureFile = "adventure/adventure.json"
+)
+
+func main() {
+	adventure, err := adventure.ParseJSON(adventureFile)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	log.Printf("%v", adventure)
 }
